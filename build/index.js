add47a3f8ebcec12f2f035e7cd1670a6fb64fd6a
PeakpoonPK <peakpoonamoroll@gmail.com>
2024-01-10 14:22:18 +0700

    abstract

--- build/index.js
+++ build/index.js
@@ -1,12 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
-const CharactersCollection_1 = require("./CharactersCollection");
-// const numbersCollection = new NumbersCollection([10,3,-5,0])
+const NumbersCollection_1 = require("./NumbersCollection");
+const LinkedList_1 = require("./LinkedList");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 // const sorter = new Sorter(numbersCollection)
 // sorter.sort();
 // console.log((numbersCollection.data));
-const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
-const sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection)
+// const charactersCollection = new CharactersCollection('Xaayb');
+// charactersCollection.sort()
+// console.log(charactersCollection.data)
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+linkedList.sort();
+linkedList.print();
