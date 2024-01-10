2c3a0af2935bc22b613f90e89da20ae8b3faf5ed
PeakpoonPK <peakpoonamoroll@gmail.com>
2024-01-10 10:23:53 +0700

    edit sort

---
+++ build/NumbersCollection.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+class NumbersCollection {
+    constructor(data) {
+        this.data = data;
+        compare();
+    }
+}
+exports.NumbersCollection = NumbersCollection;
